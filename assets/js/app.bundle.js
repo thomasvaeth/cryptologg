1a9242a570105d5e5ffec5ce7aa3ba8cc08f09a4
thomasvaeth <thomas.vaeth@gmail.com>
2017-12-13 23:42:10 -0800

    Added base of popup.

--- assets/js/app.bundle.js
+++ assets/js/app.bundle.js
@@ -34989,34 +34989,98 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = __webpack_require__(3);
 
 var _react2 = _interopRequireDefault(_react);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; } // ----------------------------------------------
+// Imports
+// ----------------------------------------------
+
+
 // ----------------------------------------------
 // Header
 // ----------------------------------------------
-var Header = function Header() {
-  var logo = ['chart-down', 'chart-up'][Math.floor(Math.random() * 2)];
+var Header = function (_Component) {
+  _inherits(Header, _Component);
 
-  return _react2.default.createElement(
-    'header',
-    { className: 'header' },
-    _react2.default.createElement(
-      'figure',
-      { className: 'header__img' },
-      _react2.default.createElement('img', { src: 'assets/images/' + logo + '.png', alt: 'CryptoLogg' })
-    )
-  );
-};
+  function Header(props) {
+    _classCallCheck(this, Header);
+
+    var _this = _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).call(this, props));
+
+    _this.state = { popup: false };
+
+    _this.logo = ['chart-down', 'chart-up'][Math.floor(Math.random() * 2)];
+    _this.togglePopup = _this.togglePopup.bind(_this);
+    return _this;
+  }
+
+  _createClass(Header, [{
+    key: 'togglePopup',
+    value: function togglePopup() {
+      this.setState(function (prevState) {
+        return {
+          popup: !prevState.popup
+        };
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      console.log(this.state);
+
+      return _react2.default.createElement(
+        'div',
+        { className: this.state.popup ? 'popup-open' : 'popup-closed' },
+        _react2.default.createElement(
+          'header',
+          { className: 'header' },
+          _react2.default.createElement(
+            'figure',
+            { className: 'header__img' },
+            _react2.default.createElement('img', { src: 'assets/images/' + this.logo + '.png', alt: 'CryptoLogg' })
+          ),
+          _react2.default.createElement(
+            'span',
+            { className: 'header__popup', onClick: this.togglePopup },
+            'About'
+          )
+        ),
+        _react2.default.createElement(
+          'section',
+          { className: 'popup' },
+          _react2.default.createElement(
+            'div',
+            { className: 'popup__close', onClick: this.togglePopup },
+            _react2.default.createElement('div', { className: 'popup__exit' })
+          ),
+          _react2.default.createElement(
+            'div',
+            { className: 'popup__container' },
+            _react2.default.createElement('div', { className: 'popup__img' }),
+            _react2.default.createElement('div', { className: 'popup__content' })
+          )
+        )
+      );
+    }
+  }]);
+
+  return Header;
+}(_react.Component);
+
+;
 
 // ----------------------------------------------
 // Exports
-// ----------------------------------------------
-// ----------------------------------------------
-// Imports
 // ----------------------------------------------
 exports.default = Header;
 
--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,21 +1,54 @@
 // ----------------------------------------------
 // Imports
 // ----------------------------------------------
-import React from 'react';
+import React, { Component } from 'react';
 
 // ----------------------------------------------
 // Header
 // ----------------------------------------------
-const Header = () => {
-  const logo = ['chart-down', 'chart-up'][Math.floor(Math.random() * 2)];
+class Header extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <header className="header">
-      <figure className="header__img">
-        <img src={`assets/images/${logo}.png`} alt="CryptoLogg"/>
-      </figure>
-    </header>
-  );
+    this.state = { popup: false };
+
+    this.logo = ['chart-down', 'chart-up'][Math.floor(Math.random() * 2)];
+    this.togglePopup = this.togglePopup.bind(this);
+  }
+
+  togglePopup() {
+    this.setState(prevState => ({
+      popup: !prevState.popup
+    }));
+  }
+
+  render() {
+    console.log(this.state);
+
+    return (
+      <div className={this.state.popup ? 'popup-open' : 'popup-closed'}>
+        <header className="header">
+          <figure className="header__img">
+            <img src={`assets/images/${this.logo}.png`} alt="CryptoLogg"/>
+          </figure>
+          <span className="header__popup" onClick={this.togglePopup}>About</span>
+        </header>
+        <section className="popup">
+          <div className="popup__close" onClick={this.togglePopup}>
+            <div className="popup__exit"></div>
+          </div>
+          <div className="popup__container">
+            <div className="popup__img">
+              
+            </div>
+            <div className="popup__content">
+
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
 };
 
 // ----------------------------------------------
