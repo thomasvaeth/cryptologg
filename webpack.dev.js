cf9d4c3da0caf696eae46e61f87baf906ffdf6d6
thomasvaeth <thomas.vaeth@gmail.com>
2018-02-26 21:06:29 -0800

    Added webpack-dashboard.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,11 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const DashboardPlugin = require('webpack-dashboard/plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   plugins: [
+    new DashboardPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html'
     })
